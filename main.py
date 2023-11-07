c6252c4b3ddd37b498ce5734d1bbcd64253f994f
Bharat-Yadav-11 <142567000+Bharat-Yadav-11@users.noreply.github.com>
2023-11-07 23:24:36 +0530

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import*
+from tkinter import *
 from tkinter import ttk
 from PIL import Image,ImageTk
 
@@ -10,7 +10,7 @@ class Face_Recognition_System:
         self.root.title("Face Recognition System")
         
         # first image
-        img = Image.open(r"C:\Users\BHARAT BHUSHAN\Downloads\Face_Recognition_Images\face2.jpg")
+        img = Image.open("Images/face2.jpg")
         img = img.resize((500,130),Image.LANCZOS)
         self.photoImg = ImageTk.PhotoImage(img)
         f_label = Label(self.root, image = self.photoImg)
@@ -19,7 +19,7 @@ class Face_Recognition_System:
 
 
         # second image  
-        img1 = Image.open(r"C:\Users\BHARAT BHUSHAN\Downloads\Face_Recognition_Images\face2.jpg")
+        img1 = Image.open("Images/face2.jpg")
         img1 = img1.resize((500,130),Image.LANCZOS)
         self.photoImg1 = ImageTk.PhotoImage(img1)
         f_label = Label(self.root, image = self.photoImg1)
@@ -27,15 +27,15 @@ class Face_Recognition_System:
 
 
         # third image
-        img2 = Image.open(r"C:\Users\BHARAT BHUSHAN\Downloads\Face_Recognition_Images\face2.jpg")
-        img2 = img.resize((500,130),Image.LANCZOS)
+        img2 = Image.open("Images/face2.jpg")
+        img2 = img2.resize((500,130),Image.LANCZOS)
         self.photoImg2 = ImageTk.PhotoImage(img2)
         f_label = Label(self.root, image = self.photoImg2)
         f_label.place(x=1000, y=0, width=500, height=130)
 
 
         # background image
-        img3 = Image.open(r"C:\Users\BHARAT BHUSHAN\Downloads\Face_Recognition_Images\face.png")
+        img3 = Image.open("Images/face.png")
         img3 = img3.resize((1510,710),Image.LANCZOS)
         self.photoImg3 = ImageTk.PhotoImage(img3)
         bg_img = Label(self.root, image = self.photoImg3)
@@ -48,8 +48,8 @@ class Face_Recognition_System:
 
 
         # Student Detail button
-        img4 = Image.open(r"C:\Users\BHARAT BHUSHAN\Downloads\Face_Recognition_Images\Student Details.jpg")
-        img4 = img.resize((220,220),Image.LANCZOS)
+        img4 = Image.open("Images/Student Details.jpg")
+        img4 = img4.resize((220,220),Image.LANCZOS)
         self.photoImg4 = ImageTk.PhotoImage(img4)
         b1 = Button(bg_img, image=self.photoImg4, cursor="hand2")
         b1.place(x=200, y=100, width=220, height=220)
@@ -60,8 +60,8 @@ class Face_Recognition_System:
 
 
         # Detect Face Button
-        img5 = Image.open(r"C:\Users\BHARAT BHUSHAN\Downloads\Face_Recognition_Images\face2.jpg")
-        img5 = img.resize((220,220),Image.LANCZOS)
+        img5 = Image.open("Images/face2.jpg")
+        img5 = img5.resize((220,220),Image.LANCZOS)
         self.photoImg5 = ImageTk.PhotoImage(img5)
         b1 = Button(bg_img, image=self.photoImg5, cursor="hand2")
         b1.place(x=500, y=100, width=220, height=220)
@@ -72,8 +72,8 @@ class Face_Recognition_System:
 
 
         # Attendance Button
-        img6 = Image.open(r"C:\Users\BHARAT BHUSHAN\Downloads\Face_Recognition_Images\face2.jpg")
-        img6 = img.resize((220,220),Image.LANCZOS)
+        img6 = Image.open("Images/face2.jpg")
+        img6 = img6.resize((220,220),Image.LANCZOS)
         self.photoImg6 = ImageTk.PhotoImage(img6)
         b1 = Button(bg_img, image=self.photoImg6, cursor="hand2")
         b1.place(x=800, y=100, width=220, height=220)
@@ -84,14 +84,61 @@ class Face_Recognition_System:
 
 
         # Help Button  
-        img7 = Image.open(r"C:\Users\BHARAT BHUSHAN\Downloads\Face_Recognition_Images\face2.jpg")
-        img7 = img.resize((220,220),Image.LANCZOS)
+        img7 = Image.open("Images/face2.jpg")
+        img7 = img7.resize((220,220),Image.LANCZOS)
         self.photoImg7 = ImageTk.PhotoImage(img7)
         b1 = Button(bg_img, image=self.photoImg7, cursor="hand2")
         b1.place(x=1100, y=100, width=220, height=220)
 
         b1 = Button(bg_img, text="Help Desk", cursor="hand2",font=("times new roman", 15, "bold"), bg="blue", fg="white")
         b1.place(x=1100, y=300, width=220, height=40)
+
+
+        # Train Face Button
+        img8 = Image.open("Images/face2.jpg")
+        img8 = img8.resize((220,220),Image.LANCZOS)
+        self.photoImg8 = ImageTk.PhotoImage(img8)
+        b1 = Button(bg_img, image=self.photoImg8, cursor="hand2")
+        b1.place(x=200, y=380, width=220, height=220)
+
+        b1 = Button(bg_img, text="Train Data", cursor="hand2",font=("times new roman", 15, "bold"), bg="blue", fg="white")
+        b1.place(x=200, y=580, width=220, height=40)
+
+
+
+        # Photos Button
+        img9 = Image.open("Images/face2.jpg")
+        img9 = img9.resize((220,220),Image.LANCZOS)
+        self.photoImg9 = ImageTk.PhotoImage(img9)
+        b1 = Button(bg_img, image=self.photoImg9, cursor="hand2")
+        b1.place(x=500, y=380, width=220, height=220)
+
+        b1 = Button(bg_img, text="Photos", cursor="hand2",font=("times new roman", 15, "bold"), bg="blue", fg="white")
+        b1.place(x=500, y=580, width=220, height=40)
+
+
+
+        # Developer Button
+        img10 = Image.open("Images/face2.jpg")
+        img10 = img10.resize((220,220),Image.LANCZOS)
+        self.photoImg10 = ImageTk.PhotoImage(img10)
+        b1 = Button(bg_img, image=self.photoImg10, cursor="hand2")
+        b1.place(x=800, y=380, width=220, height=220)
+
+        b1 = Button(bg_img, text="Developer", cursor="hand2",font=("times new roman", 15, "bold"), bg="blue", fg="white")
+        b1.place(x=800, y=580, width=220, height=40)
+
+
+
+        # Exit Button
+        img11 = Image.open("Images/face2.jpg")
+        img11 = img11.resize((220,220),Image.LANCZOS)
+        self.photoImg11 = ImageTk.PhotoImage(img11)
+        b1 = Button(bg_img, image=self.photoImg11, cursor="hand2")
+        b1.place(x=1100, y=380, width=220, height=220)
+
+        b1 = Button(bg_img, text="Photos", cursor="hand2",font=("times new roman", 15, "bold"), bg="blue", fg="white")
+        b1.place(x=1100, y=580, width=220, height=40)
 
 
 
